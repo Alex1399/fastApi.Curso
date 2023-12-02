f6457840130500efc344078442ee976ffc2de32b
Alex1399 <jhhoni00@gmail.com>
2023-12-02 00:21:37 -0500

    Cambios por Alex

--- practica_activa/api_producto.py
+++ practica_activa/api_producto.py
@@ -1,6 +1,7 @@
 from typing import Optional
-from fastapi import FastAPI
+from uuid import uuid4 as uuid
 
+from fastapi import FastAPI, HTTPException
 from pydantic import BaseModel
 
 class Producto(BaseModel):
@@ -14,6 +15,7 @@ class Producto(BaseModel):
 app= FastAPI()
 
 productos= []
+
 @app.get('/')
 def index():
     return {'mensaje':'Binevenh'}
@@ -24,6 +26,18 @@ def obtener_productos():
 
 @app.post('/producto')
 def crear_producto(producto: Producto):
+    producto.id = str(uuid())
     productos.append(producto)
     return{'mensaje':'Producto creado satisfactoriamente'}
+
+@app.get('/producto/{producto_id}')
+def obtener_producto_por_id(producto_id: str):
+    resultado= list(filter(lambda p: p.id == producto_id, productos))
+    
+    if len(resultado):
+        return resultado[0]
+    
+    raise HTTPException(status_code=404, detail= f'el producto con el ID {producto_id} no fue encontrado')
+
+    
 
