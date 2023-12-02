8c0e5829bdeb6beb3bb80e684acd9f5fd021ad7c
Alex1399 <jhhoni00@gmail.com>
2023-12-01 23:24:34 -0500

    Cambios Por Alex

--- practica_activa/api_producto.py
+++ practica_activa/api_producto.py
@@ -1,7 +1,29 @@
+from typing import Optional
 from fastapi import FastAPI
 
+from pydantic import BaseModel
+
+class Producto(BaseModel):
+    id: Optional[str]
+    nombre: str
+    prec_compra: float
+    prec_venta: float
+    proveedor: str
+    
+
 app= FastAPI()
+
 productos= []
 @app.get('/')
 def index():
     return {'mensaje':'Binevenh'}
+
+@app.get('/producto')
+def obtener_productos():
+    return productos
+
+@app.post('/producto')
+def crear_producto(producto: Producto):
+    productos.append(producto)
+    return{'mensaje':'Producto creado satisfactoriamente'}
+
